dccf56a85efceeb3f611078716388a167d40a068
sagar patel <sagar@studytoursindia.com>
2020-07-13 23:40:29 +0530

    auth bugs removed and changes applied

---
+++ js/init-firebase.js
@@ -0,0 +1,21 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDKhu8wHKsZrtxxPly6JsUWU5GEmqTeQbU",
+    authDomain: "database-for-igil.firebaseapp.com",
+    databaseURL: "https://database-for-igil.firebaseio.com",
+    projectId: "database-for-igil",
+    storageBucket: "database-for-igil.appspot.com",
+    messagingSenderId: "351887413642",
+    appId: "1:351887413642:web:a0af1715a2475fbe477873",
+    measurementId: "G-D4CDDQ6VF2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
+ 
+
+  // window.onload = function() {
+  //   initApp();
+  // };
